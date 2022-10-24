b0af59deb0d4d95e800f05dbd1f757b990163e43
life ⚡ <life@shellz.wtf>
2022-10-23 18:45:34 -0700

    ⚡️🧠⚡️ - Laizy AI

---
+++ README.md
@@ -0,0 +1,60 @@
+
+# Laizy Shell
+
+An interactive shell environment with an enhanced AI assistant. All the power of laizy.dev in a handy commandline tool.
+
+<!-- image of laizy-shell -->
+<img src="./laizy-shell.png" alt="Laizy Shell" width="800"/>
+
+## Features
+
+- **Natural language understanding:** laizy understands you and can answer your questions
+- **Versatile input:** laizy is a general purpose assistant, it can answer questions about anything (engineering, science, history, etc.)
+- **Easy installation:** just download laizy and you're good to go
+- **Made with love:** laizy is designed to be easy to use, and make the power of AI accessible to everyone
+
+
+
+
+
+
+
+# Installation
+Laizy is written in Go, so you'll need to have Go installed on your machine. You can download Go [here](https://golang.org/dl/). After that you can install laizy by running the following command:
+
+```bash
+go get github.com/laizy-ai/laizy-shell
+```
+
+Alternatively, you can download the latest release from the [releases page](https://github.com/laizy-ai/laizy-shell/releases).
+
+
+
+# Usage
+
+The Laizy CLI provides a number of commands to help you be more productive. You can type a prompt directly in directly, load a file, or use the multi-line interactive mode.
+
+Head over to https://app.laizy.dev to sign-up and get an API key.   
+
+You'll need to set the LAIZY_API_KEY environment variable to your API key. You can do this by running the following command:
+
+```
+export LAIZY_API_KEY=YOUR_API_KEY
+```
+
+Next, you can run the following command to start the Laizy Shell:
+` laizy-shell `
+
+```
+help - Show help
+clear - Clear the screen
+exit - Exit the shell
+
+%multi - Toggle multi-line mode
+%load - Load a prompt from file, e.g. %load prompt.txt
+%save - Save prompt output to a file, e.g. %save output.txt
+%exec - Execute a shell command
+%history - Show prompt history
+```
+
+<img src="./laizy-shell.gif" alt="Laizy Shell" width="800"/>
---
+++ main.go
@@ -0,0 +1,327 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/Jeffail/gabs"
+	"github.com/pterm/pterm"
+)
+
+var (
+	laizyAPIKey            = os.Getenv("LAIZY_API_KEY")
+	promptHistory          = []string{}
+	userHomeFolder         = os.Getenv("HOME")
+	promptHistoryFile      = fmt.Sprintf("%s/.config/laizy/.prompt_history", userHomeFolder)
+	promptValue            = ""
+	lastPrompt             = ""
+	laizyInputMultiLine    = false
+	laizyInputFile         string
+	laizyInputFileContents string
+	laizyFullResponse      = ""
+	laizyLastResponse      = ""
+	laizyQOTDMessages      = []string{
+		"You are a star!",
+		"Keep up the good work!",
+		"Your hard work will pay off!",
+		"You are doing great!",
+		"You are a rockstar!",
+		"You are a genius!",
+		"You are a superstar!",
+		"You are a legend!",
+		"You are a champion!",
+	}
+	helpMenuEntries = []string{
+		"Laizy CLI",
+		"Type exit or quit to exit",
+		"Type clear to clear the screen",
+		"Type help to show this menu",
+		"Type %load to load prompt from a file",
+		"Type %save to save the current output to a file",
+		"Type %exec to execute a shell command",
+		"Type %history to show the command history",
+		"Type %multi to toggle multiline mode",
+	}
+	bannerQOTD = ""
+	clear      map[string]func() //create a map for storing clear funcs
+
+)
+
+func CallClear() {
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Your platform is unsupported! I can't clear terminal screen :(")
+	}
+}
+func init() {
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["darwin"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	rand.Seed(time.Now().Unix())
+	randomBannerMessage := laizyQOTDMessages[rand.Intn(len(laizyQOTDMessages))]
+	bannerQOTD = pterm.NewStyle(pterm.FgLightMagenta).Sprint(randomBannerMessage)
+
+	// attempt to create laizy config folder
+	laizyConfigFolder := fmt.Sprintf("%s/.config/laizy", userHomeFolder)
+	_, err := os.Stat(laizyConfigFolder)
+	if os.IsNotExist(err) {
+		os.Mkdir(laizyConfigFolder, 0755)
+	}
+
+	// read prompt history file
+	promptHistoryFile, err := os.ReadFile(promptHistoryFile)
+	if err != nil {
+		pterm.Error.Println(err)
+	}
+	promptHistory = strings.Split(string(promptHistoryFile), "\n")
+}
+
+func specialCommandHandler(userPrompt string) bool {
+	unmodifiedPrompt := userPrompt
+	userPrompt = strings.Split(userPrompt, " ")[0]
+
+	if userPrompt == "%multi" {
+		// toggle multi line input
+		laizyInputMultiLine = !laizyInputMultiLine
+		if laizyInputMultiLine {
+			pterm.Info.Println("Multi line input enabled")
+		} else {
+			pterm.Info.Println("Single line input enabled")
+		}
+
+		return true
+	}
+	if userPrompt == "%clear" {
+		CallClear()
+		return true
+	}
+	if userPrompt == "%exit" || userPrompt == "%quit" || userPrompt == "exit" || userPrompt == "quit" {
+		os.Exit(0)
+		return true
+	}
+	if userPrompt == "%help" || userPrompt == "help" {
+
+		for _, entry := range helpMenuEntries {
+			pterm.Info.Println(entry)
+		}
+
+		return true
+	}
+	if userPrompt == "%history" {
+		pterm.Info.Println("Laizy CLI Prompt History")
+		for index, prompt := range promptHistory {
+			pterm.Info.Println(index, prompt)
+		}
+		return true
+	}
+	if userPrompt == "%load" || userPrompt == "%ld" {
+		if len(strings.Split(unmodifiedPrompt, " ")) > 1 {
+			laizyInputFile = strings.Split(unmodifiedPrompt, " ")[1]
+			laizyInputFile = strings.TrimSpace(laizyInputFile)
+		} else {
+			laizyInputFile, _ = pterm.DefaultInteractiveTextInput.Show("Enter a filename to load the prompt from")
+		}
+
+		laizyInputFileContents, err := os.ReadFile(laizyInputFile)
+		if err != nil {
+			pterm.Error.Println("Error loading file", err)
+		}
+		promptValue = string(laizyInputFileContents)
+		lastPrompt = promptValue
+		laizyFullResponse = ""
+		pterm.Println("loaded prompt from file\n", promptValue)
+		return true
+	}
+	if userPrompt == "%exec" || userPrompt == "%!" {
+		// execute shell command
+		if len(strings.Split(unmodifiedPrompt, " ")) > 1 {
+			// trim trailing spaces
+			baseCommand := strings.Split(unmodifiedPrompt, " ")[1]
+			shellCommandWithArgs := strings.Split(unmodifiedPrompt, " ")[2:]
+			out, err := exec.Command(baseCommand, shellCommandWithArgs...).Output()
+			if err != nil {
+				pterm.Error.Println(err)
+			}
+			pterm.Println(string(out))
+		} else {
+			pterm.Error.Println("No shell command provided")
+		}
+		return true
+	}
+	if userPrompt == "%save" || userPrompt == "%s" {
+		var laizyOutputFile string
+		if len(strings.Split(unmodifiedPrompt, " ")) > 1 {
+			laizyOutputFile = strings.Split(unmodifiedPrompt, " ")[1]
+		} else {
+			laizyOutputFile, _ = pterm.DefaultInteractiveTextInput.Show("Enter a filename to save the last response to")
+		}
+		pterm.Info.Println("Saving prompt output to file", laizyOutputFile)
+		// f, err := os.Create(laizyOutputFile)
+		// file exists
+		f, err := os.OpenFile(laizyOutputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			pterm.Error.Println("Error creating file", err, laizyOutputFile)
+		}
+		defer f.Close()
+		_, err = io.WriteString(f, laizyFullResponse)
+		if err != nil {
+			pterm.Error.Println("Error writing to file", laizyOutputFile)
+		}
+		return true
+	}
+
+	if userPrompt == "%qotd" {
+		pterm.Info.Println(bannerQOTD)
+		return true
+	}
+
+	if regexp.MustCompile(`^%`).MatchString(userPrompt) {
+		return true
+	}
+	return false
+}
+
+func main() {
+
+	// clear screen on linux
+	CallClear()
+	if laizyAPIKey == "" {
+		pterm.Error.Println("Laizy API Key not set - please set LAIZY_API_KEY environment variable")
+		pterm.Error.Println("You can get a free API key at https://app.laizy.dev")
+		os.Exit(1)
+	}
+	// show laizy.dev header
+	pterm.DefaultHeader.WithFullWidth().WithBackgroundStyle(pterm.NewStyle(pterm.BgLightBlue)).WithTextStyle(pterm.NewStyle(pterm.FgLightYellow)).WithMargin(5).Println("Laizy AI")
+	pterm.DefaultHeader.WithFullWidth().WithBackgroundStyle(pterm.NewStyle(pterm.BgLightBlue)).WithMargin(5).Println(bannerQOTD)
+	// show laiz.dev logo
+	pterm.DefaultCenter.Println(laizyLogo)
+	// print sample prompts for users
+	pterm.DefaultCenter.Println(pterm.NewStyle(pterm.FgLightMagenta).Sprint("Try some of these prompts:"))
+	pterm.DefaultCenter.Println(pterm.NewStyle(pterm.FgLightMagenta).Sprint("Generate some golang code for a web server"))
+	pterm.DefaultCenter.Println(pterm.NewStyle(pterm.FgLightMagenta).Sprint("Generate a bash script to install a web server"))
+	pterm.DefaultCenter.Println(pterm.NewStyle(pterm.FgLightMagenta).Sprint("Generate a bash script to install a web server that uses a database"))
+
+	// main repl loop
+	for {
+		var laizySpinnerMessage = "thinking"
+		var laizyResponseJSON map[string]interface{}
+		// show user prompt
+		// text style for a blue background
+
+		inputPromptStyle := pterm.NewStyle(pterm.FgLightYellow, pterm.BgLightBlue)
+		var userPromptValue string
+		if laizyInputMultiLine {
+			userPromptValue, _ = pterm.DefaultInteractiveTextInput.WithMultiLine().WithTextStyle(inputPromptStyle).Show("LAIZY>")
+		} else {
+			userPromptValue, _ = pterm.DefaultInteractiveTextInput.WithTextStyle(inputPromptStyle).Show("LAIZY>")
+		}
+		if specialCommandHandler(userPromptValue) {
+			continue
+		}
+
+		// if user prompt is blank treat it as a continuation of the prompt + response
+		if len(userPromptValue) == 0 {
+			if len(laizyLastResponse) != 0 {
+				// continue from last prompt
+				promptValue = lastPrompt + laizyFullResponse
+				laizySpinnerMessage = "continuing from last response"
+				// clear screen
+
+			} else {
+				// for loading data from a file
+				if laizyInputFileContents != "" {
+					pterm.Info.Println("Loading data from file", laizyInputFile)
+
+					promptValue = userPromptValue
+					laizyInputFileContents = ""
+					laizyInputFile = ""
+				} else {
+					laizyFullResponse = ""
+
+				}
+			}
+		} else {
+			promptValue = userPromptValue
+			lastPrompt = userPromptValue
+			laizyFullResponse = ""
+			promptHistory = append(promptHistory, userPromptValue)
+			// write prompt history to file
+			f, err := os.Create(promptHistoryFile)
+			if err != nil {
+				pterm.Error.Println("Error creating file", promptHistoryFile)
+			}
+			_, err = io.WriteString(f, strings.Join(promptHistory, "\n"))
+
+		}
+		// show the loading spinner
+		spinnerInfo, _ := pterm.DefaultSpinner.Start(fmt.Sprintf("Laizy is %s...", laizySpinnerMessage))
+		// send the request to laizy.dev
+		laizyResponse := sendLaizyRequest(promptValue, 1)
+		// retrieve the response from laizy.dev
+		err := json.Unmarshal([]byte(laizyResponse), &laizyResponseJSON)
+		if err != nil {
+			spinnerInfo.Fail("An error occured")
+		} else {
+			spinnerInfo.Success()
+			if laizyResponseJSON["content"] != nil {
+				laizyLastResponse = laizyResponseJSON["content"].(string)
+				laizyFullResponse = laizyFullResponse + laizyLastResponse
+				pterm.Println(pterm.NewStyle(pterm.FgLightCyan).Sprint(laizyFullResponse)) // main loop
+			} else {
+				pterm.Error.Println("Laizy was unable to process your request")
+			}
+		}
+	}
+}
+
+// A JSON blob containing all us presidents and their party affiliation
+func sendLaizyRequest(userPrompt string, iterations int) string {
+	url := "https://app.laizy.dev/submit"
+
+	laizyRequestBody := gabs.New()
+	laizyRequestBody.Set(userPrompt, "prompt")
+	laizyRequestBody.Set(iterations, "iterations")
+	laizyReader := strings.NewReader(laizyRequestBody.String())
+
+	req, err := http.NewRequest("POST", url, laizyReader)
+	if err != nil {
+		pterm.Error.Println(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-KEY", laizyAPIKey)
+	req.Header.Set("User-Agent", "laizy-cli")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		pterm.Error.Println(err)
+	}
+	// defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		pterm.Error.Println(err)
+	}
+	return (string(body))
+}
